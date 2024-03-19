34067f19d8b2afaf9b553b3f011102b44594521e
Joshua Sparkes <109252401+joshuasparkes@users.noreply.github.com>
2024-03-19 16:31:18 +0000

    Create flightSearch.js

---
+++ flightSearch.js
@@ -0,0 +1,12 @@
+const FlightSearch = () => {
+  return (
+    <div>
+    <p>Origin<p>
+    <p>Destination<p>
+    <p>Outbound<p>
+    <p>Return<p>
+    <div>
+    )
+    }
+export default FlightSearch
+    
