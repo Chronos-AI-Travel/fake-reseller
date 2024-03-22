e84d069f02d1c7e4d0856fccb7bcabf55332fdc9
Joshua Sparkes <109252401+joshuasparkes@users.noreply.github.com>
2024-03-22 11:09:49 +0000

    Create or update flightSearch.js

--- flightSearch.js
+++ flightSearch.js
@@ -1,15 +1,41 @@
-import FlightResults from '/flightResults'
 
-const FlightSearch = () => {
-  return (
-    <div>
-    <p>Origin<p>
-    <p>Destination<p>
-    <p>Outbound<p>
-    <p>Return<p>
-    <FlightResults/>
-    <div>
-    )
+</p><pre><code>
+import React, { useState } from 'react';
+import axios from 'axios';
+import FlightResults from '/flightResults';
+
+const FlightSearch = () =&gt; {
+  const [origin, setOrigin] = useState('');
+  const [destination, setDestination] = useState('');
+  const [outbound, setOutbound] = useState('');
+  const [returnDate, setReturnDate] = useState('');
+  const [flights, setFlights] = useState([]);
+
+  const searchFlights = async () =&gt; {
+    try {
+      const response = await axios.post('Duffel_API_URL', {
+        origin: origin,
+        destination: destination,
+        outbound: outbound,
+        return: returnDate
+      });
+      setFlights(response.data);
+    } catch (error) {
+      console.error('Error searching for flights:', error);
     }
-export default FlightSearch
-    
+  };
+
+  return (
+    &lt;div&gt;
+      &lt;input type="text" placeholder="Origin" value={origin} onChange={(e) =&gt; setOrigin(e.target.value)} /&gt;
+      &lt;input type="text" placeholder="Destination" value={destination} onChange={(e) =&gt; setDestination(e.target.value)} /&gt;
+      &lt;input type="date" placeholder="Outbound" value={outbound} onChange={(e) =&gt; setOutbound(e.target.value)} /&gt;
+      &lt;input type="date" placeholder="Return" value={returnDate} onChange={(e) =&gt; setReturnDate(e.target.value)} /&gt;
+      &lt;button onClick={searchFlights}&gt;Search Flights&lt;/button&gt;
+      {flights.length &gt; 0 && &lt;FlightResults flights={flights} /&gt;}
+    &lt;/div&gt;
+  );
+};
+
+export default FlightSearch;
+</code></pre><p></p>
